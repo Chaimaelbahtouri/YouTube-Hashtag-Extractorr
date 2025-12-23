f4556d426a0409ee5890cc7c39ad8092a9322c06
Chaimaelbahtouri <chaimaaelbahtouri@gmail.com>
2025-12-23 02:00:21 +0100

    Update API and frontend fixes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ['http://localhost:3000', 'https://you-tube-hashtag-extractorr-76s708lov-cels-projects-e17f6168.vercel.app']
+  }
+}
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  allowedDevOrigins: ['http://192.168.136.1:3000'], // بدّل هاد الـ IP باللي كتستعمله فالـ dev
-}
-
-module.exports = nextConfig;
